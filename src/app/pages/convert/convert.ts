3da1b6f987ec2bbcbd825f839f7e77f87e02f4ac
Filip Bergander <filip.bergander1996@gmail.com>
2026-04-22 09:55:48 +0200

    Lagt till innehåll i notFound-sidan. Skapat logik för konvertering.

--- src/app/pages/convert/convert.ts
+++ src/app/pages/convert/convert.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-convert',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './convert.html',
   styleUrl: './convert.scss',
 })
-export class Convert {}
+export class Convert {
+  convCelsiusToFahrenheit(celsius: number): number {
+    return (celsius * 1.8) + 32;
+  }
+
+  convFahrenheitToCelsius(fahrenheit:number): number {
+    return (fahrenheit -32 )/ 1.8;
+  }
+
+  convFeetToMeter (feet:number): number {
+    return (feet * 0.3048);
+  }
+
+    convMeterToFeet (meter:number): number {
+    return (meter * 3.28084);
+  }
+
+  constructor() {
+  }
+}
