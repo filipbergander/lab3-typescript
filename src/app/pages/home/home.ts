8590d911ccd2952967d7979bdbdfeda21a3ab5b0
Filip Bergander <filip.bergander1996@gmail.com>
2026-04-23 10:57:31 +0200

    Experimenterar lite med pipes för nummer.

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Convert } from "../convert/convert";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Convert],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
